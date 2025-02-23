71cf45e560f052e1adf7d45153d4042f8fe5b7c8
syaquiii <msyauqioqi@gmail.com>
2025-02-23 16:17:26 +0700

    Setup Folder Structure and Page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface IProps {}
+
+const page: FC<IProps> = (props) => {
+  return <div className="">Welcome to Home Page</div>;
+};
+
+export default page;
