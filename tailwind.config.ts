da26b982eb07b95ef5afa6d9200b924cde750881
syaquiii <msyauqioqi@gmail.com>
2025-02-26 13:59:33 +0700

    setting up tailwindcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ export default {
         foreground: "var(--foreground)",
         wfgray: "#D9D9D9",
         wfblack: "#000000 ",
+        light: {
+          default: "#ebf3ff",
+          hover: "#e1edfe",
+          activer: "#c0d9fe",
+        },
+        normal: {
+          default: "#3484fb",
+          hover: "#2f77e2",
+          active: "#2a6ac9",
+        },
+        dark: {
+          default: "#2763bc",
+          hover: "#1f4f97",
+          active: "#173b71",
+        },
+        darker: {
+          default: "#122e58",
+        },
+      },
+      fontFamily: {
+        poppins: [],
       },
     },
   },
