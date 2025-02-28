a55348ea2763687769f272589587392e63bfd789
syaquiii <msyauqioqi@gmail.com>
2025-02-28 16:54:11 +0700

    FIX : Revision Mobile Navbar

--- src/app/home/components/Layanan.tsx
+++ src/app/home/components/Layanan.tsx
@@ -11,24 +11,28 @@ const Layanan = () => {
         <div className="lg:flex mt-10 lg:mt-0  gap-4 w-full justify-between text-black">
           <div className="lg:w-3/4">
             <div className="layanan1 bg-gray-200 rounded-3xl h-32 text-xs 2xl:text-lg lg:h-44 p-4 lg:p-2 lg:text-sm xl:p-8">
+              <h4 className="text-lg font-bold">Kelas</h4>
               Kelas interaktif dengan pengalaman belajar yang mendorong
               eksplorasi ide, problem-solving, dan pengembangan keterampilan
               seru dan kolaboratif.
             </div>
             <div className="flex gap-4 mt-4">
-              <div className="h-52 layanan2 bg-gray-200 w-full text-xs   2xl:text-lg rounded-3xl p-4 lg:p-2 lg:text-sm xl:p-8">
+              <div className="h-52 layanan2 bg-gray-200 w-full text-xs   2xl:text-lg rounded-3xl p-4 lg:p-2 lg:text-sm xl:px-8 xl:py-6">
+                <h4 className="text-lg font-bold">Mentor</h4>
                 Mentor berpengalaman di bidangnya yang siap membimbingmu dengan
                 pendekatan interaktif dan personal.
               </div>
               <div className="h-52 layanan3 bg-gray-200 w-full rounded-3xl 2xl:text-lg text-xs p-4 lg:p-2 lg:text-sm xl:p-8">
+                <h4 className="text-lg font-bold">Ruang Diskusi</h4>
                 Ruang diskusi yang seru untuk bertukar ide, pengalaman, dan
                 perspektif!
               </div>
             </div>
           </div>
           <div className="bg-gray-200 layanan4 mt-4 h-32 lg:h-full lg:mt-0 2xl:text-lg text-xs lg:w-1/4 rounded-3xl lg:p-2 lg:text-sm xl:p-8 p-4">
-            Sebuah jembatan antara inovator dan investor yang siap mendukung
-            ide-ide brilianmu.
+            Innova Space adalah platform belajar interaktif yang menghubungkan
+            mentor dan pembelajar untuk pengembangan skill dan wawasan secara
+            fleksibel.
           </div>
         </div>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,10 +47,11 @@ const Navbar = () => {
     return (
       <div
         className={`${
-          isOpen ? "" : "opacity-0 -top-20"
-        } transition-all duration-300 w-full bg-light-default h-screen -mt-4`}
+          isOpen &&
+          "bg-black bg-opacity-35 flex justify-end -mt-4 w-full h-screen"
+        }`}
       >
-        <div className=" mt-32 ml-6 w-2/3">
+        <div className={`w-2/3  px-10 py-20 h-[100vh] bg-light-default `}>
           <ul className="flex gap-6 font-bold text-xl  flex-col">
             {navlink.map((item) => (
               <li
@@ -66,7 +67,7 @@ const Navbar = () => {
             ))}
           </ul>
           <Link href={"/login"}>
-            <Button className="mt-4" variant="normal" size="lg">
+            <Button className="mt-4 px-8" variant="normal" size="normal">
               Masuk
             </Button>
           </Link>
@@ -81,7 +82,7 @@ const Navbar = () => {
         isVisible ? "top-0" : "opacity-0 -top-20"
       } transition-all duration-300 fixed flex z-10 justify-end lg:justify-center w-full mt-4 lg:mt-0`}
     >
-      {MobileNavbar()}
+      {isOpen && MobileNavbar()}
 
       <div className="hidden fixed lg:w-full xl:w-[70rem] mycontainer mt-10 font-semibold lg:text-md xl:text-xl bg-light-default lg:flex justify-between h-20 rounded-3xl lg:px-8 items-center">
         <Logo size="normal" style="dark" />
@@ -107,7 +108,7 @@ const Navbar = () => {
       </div>
       <div
         className={` lg:hidden m-4 rounded-lg ${
-          isOpen ? "bg-light-default" : "bg-normal-default"
+          isOpen ? "bg-opacity-0" : "bg-normal-default"
         } text-white fixed`}
       >
         <Hamburger
