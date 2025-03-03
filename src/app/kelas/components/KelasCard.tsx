ebd821cf8229e9a0c763f11ad68ad61afd65d70f
syaquiii <msyauqioqi@gmail.com>
2025-03-04 02:47:18 +0700

    FIX : Kelas Section mobile fix

--- src/app/kelas/components/KelasCard.tsx
+++ src/app/kelas/components/KelasCard.tsx
@@ -10,7 +10,7 @@ const KelasCard: FC<KelasCard> = (kelas) => {
     <div className="w-full lg:flex-col  hover:scale-105 transition-all hover:bg-light-hover cursor-pointer lg:h-[20rem] lg:items-start bg-light-default overflow-hidden lg:p-4 p-2 md:p-6 min-h-24  gap-2 rounded-xl flex items-center">
       <div className="w-2/6 lg:w-full lg:h-2/3   aspect-square h-fit rounded-lg  bg-normal-default"></div>
       <div className="w-4/6 lg:w-full">
-        <h4 className="font-bold text-sm md:text-2xl truncate  lg:truncate">
+        <h4 className="font-bold text-sm md:text-2xl   lg:truncate">
           💻 {kelas.kelas.nama}
         </h4>
         <span className="text-xs md:text-sm px-2 py-1 rounded-sm bg-light-hover shadow-sm">
