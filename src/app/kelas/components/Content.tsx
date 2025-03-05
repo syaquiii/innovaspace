4387dfa3e4a52a8fdea068b18b658ea65ddbdca6
syaquiii <msyauqioqi@gmail.com>
2025-03-05 15:22:48 +0700

    ADD: Adjusting Page Kelas, ADD: slide animation on MobileFilter

--- src/app/kelas/components/Content.tsx
+++ src/app/kelas/components/Content.tsx
@@ -8,7 +8,7 @@ interface ContentProps {
 }
 const Content: React.FC<ContentProps> = ({ courses }) => {
   return (
-    <div className="lg:w-3/4 ">
+    <div className="lg:w-3/4 w-full">
       {courses.length === 0 && <NotFoundCourse />}
       <div className="lg:flex w-full  lg:w-full  justify-between gap-8 ">
         <div className="w-full grid-cols-1  grid lg:w-full lg:grid-cols-2 gap-10">
